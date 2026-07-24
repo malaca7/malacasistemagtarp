ad31f8c92e012949440839393b1aee2ccf22e3ee
malaca7 <rogeriomalaquiasjr@gmail.com>
2026-07-23 21:08:04 -0300

    Fix Vite configuration for multi-page build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/malacasistemagtarp/', // Caminho base necessário para o deploy no GitHub Pages
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        lockpick: resolve(__dirname, 'lockpick.html'),
+        caixinha: resolve(__dirname, 'caixinha.html'),
+        hacking: resolve(__dirname, 'hacking.html')
+      }
+    }
   }
 });
